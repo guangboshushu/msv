d4a272c082c253596bac3dbc7064dd57563dba55
guangboshushu <h28846019@126.coml>
2018-04-19 18:27:31 +0800

    add

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const sendMail = require('./mail/mail')
+
+
+app.post('/send_mail', (req, res) => {
+  let toUser = req.body.toUser
+  let content = req.body.content
+  let subject = req.body.content
+  sendMail(toUser, subject, content)
+  res.send(req.query)
+})
+
+
+let port = 8181
+app.listen(port, (req, res) => {
+  console.log('-----------------------------------------------------------------------------------------------')
+  console.log('|                               mail server is start, the port is : ' + port + '                            |')
+  console.log('-----------------------------------------------------------------------------------------------')
+})
---
+++ mail/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+       //邮件配置
+    email: {
+        service: 'goden',
+        user: 'webmaster@goden.cn',
+        pass: 'Goden2018%*',
+    }
+}
