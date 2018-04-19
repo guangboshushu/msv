384be808bd12123c0b745657f9f8be6ee7aedb53
guangboshushu <h28846019@126.coml>
2018-04-19 21:37:11 +0800

    add

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
+
 const express = require('express')
 const app = express()
-const sendMail = require('./mail/mail')
-const multipart = require('connect-multiparty')
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.post('/send_mail', urlencodedParser, function(req, res) {
+    if (!req.body) return res.sendStatus(400);
 
-const bodyParser = require('body-parser')
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+    console.log('Username: ' + req.body);
 
-app.use(multipart())
-
-app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({
-//   extended: false
-// }))
-
-app.post('/send_mail', urlencodedParser, (req, res) => {
-  console.log('body', req.body)
-  let toUser = req.body.toUser
-  let content = req.body.content
-  let subject = req.body.subject
-  sendMail(toUser, subject, content)
-  res.send(req.body)
+    res.send('Welcome,');
 })
 
-
+// const express = require('express')
+// const app = express()
+// const sendMail = require('./mail/mail')
+// const multipart = require('connect-multiparty')
+//
+// const bodyParser = require('body-parser')
+// const urlencodedParser = bodyParser.urlencoded({ extended: false })
+//
+// app.use(multipart())
+//
+// app.use(bodyParser.json())
+// // app.use(bodyParser.urlencoded({
+// //   extended: false
+// // }))
+//
+// app.post('/send_mail', urlencodedParser, (req, res) => {
+//   console.log('body', req.body)
+//   let toUser = req.body.toUser
+//   let content = req.body.content
+//   let subject = req.body.subject
+//   sendMail(toUser, subject, content)
+//   res.send(req.body)
+// })
+//
+//
 let port = 8181
 app.listen(port, (req, res) => {
   console.log('-----------------------------------------------------------------------------------------------')
